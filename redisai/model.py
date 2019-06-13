b5144e2b2c38d786303019484cd293c2f0aa0c1f
hhsecond <sherinct@live.com>
2019-06-13 11:12:13 +0530

    onnx support

--- redisai/model.py
+++ redisai/model.py
@@ -1,26 +1,35 @@
-import pickle
 import os
 import warnings
-
-from .client import Device, Backend
+import sys
 
 try:
     import tensorflow as tf
 except (ModuleNotFoundError, ImportError):
-    pass  # that's Okey if you don't have TF
+    pass
 
 try:
     import torch
 except (ModuleNotFoundError, ImportError):
-    pass  # it's Okey if you don't have PT either
+    pass
 
+
+try:
+    import onnx
+except (ModuleNotFoundError, ImportError):
+    pass
+
+try:
+    import skl2onnx
+    import sklearn
+except (ModuleNotFoundError, ImportError):
+    pass
 
 
 class Model:
 
     __slots__ = ['graph', 'backend', 'device', 'inputs', 'outputs']
-    
-    def __init__(self, path, device=Device.cpu, inputs=None, outputs=None):
+
+    def __init__(self, path, device=None, inputs=None, outputs=None):
         """
         Declare a model suitable for passing to modelset
         :param path: Filepath from where the stored model can be read
@@ -37,9 +46,9 @@ class Model:
         raise NotImplementedError('Instance creation is not impelemented yet')
 
     @classmethod
-    def save(cls, obj, path: str, input=None, output=None, as_native=True):
+    def save(cls, obj, path: str, input=None, output=None, as_native=True, prototype=None):
         """
-        Infer the backend (TF/PyTorch) by inspecting the class hierarchy
+        Infer the backend (TF/PyTorch/ONNX) by inspecting the class hierarchy
         and calls the appropriate serialization utility. It is essentially a
         wrapper over serialization mechanism of each backend
         :param path: Path to which the graph/model will be saved
@@ -54,15 +63,25 @@ class Model:
             mechanism if True. If False, custom saving utility will be called
             which saves other informations required for modelset. Defaults to True
         """
-        if issubclass(type(obj), tf.Session):
+        if 'tensorflow' in sys.modules and issubclass(type(obj), tf.Session):
             cls._save_tf_graph(obj, path, output, as_native)
-        elif issubclass(type(type(obj)), torch.jit.ScriptMeta):
+        elif 'torch' in sys.modules and issubclass(
+                type(type(obj)), torch.jit.ScriptMeta):
             # TODO Is there a better way to check this
-            cls._save_pt_graph(obj, path, as_native)
+            cls._save_torch_graph(obj, path, as_native)
+        elif 'onnx' in sys.modules and issubclass(
+                type(obj), onnx.onnx_ONNX_RELEASE_ml_pb2.ModelProto):
+            cls._save_onnx_graph(obj, path, as_native)
+        elif 'skl2onnx' in sys.modules and issubclass(
+                type(obj), sklearn.base.BaseEstimator):
+            cls._save_sklearn_graph(obj, path, as_native, prototype)
         else:
-            raise TypeError(('Invalid Object. '
-                'Need traced graph or scripted graph from PyTorch or '
-                'Session object from Tensorflow'))
+            message = ("Could not find the required dependancy to export the graph object. "
+                       "`save_model` relies on serialization mechanism provided by the"
+                       " supported backends such as Tensorflow, PyTorch, ONNX or skl2onnx. "
+                       "Please install package required for serializing your graph. "
+                       "For more information, checkout the redisia-py documentation")
+            raise RuntimeError(message)
 
     @classmethod
     def _save_tf_graph(cls, sess, path, output, as_native):
@@ -81,10 +100,10 @@ class Model:
             raise NotImplementedError('Saving non-native graph is not supported yet')
 
     @classmethod
-    def _save_pt_graph(cls, graph, path, as_native):
+    def _save_torch_graph(cls, graph, path, as_native):
         # TODO how to handle the cpu/gpu
         if as_native:
-            if graph.training == True:
+            if graph.training is True:
                 warnings.warn(
                     'Graph is in training mode. Converting to evaluation mode')
                 graph.eval()
@@ -93,25 +112,33 @@ class Model:
         else:
             raise NotImplementedError('Saving non-native graph is not supported yet')
 
-    @staticmethod
-    def _get_filled_dict(graph, backend, input=None, output=None):
-        return {
-            'graph': graph,
-            'backend': backend,
-            'input': input,
-            'output': output}
-
-    @staticmethod
-    def _write_custom_model(outdict, path):
-        with open(path, 'wb') as file:
-            pickle.dump(outdict, file)
+    @classmethod
+    def _save_onnx_graph(cls, graph, path, as_native):
+        if as_native:
+            with open(path, 'wb') as f:
+                f.write(graph.SerializeToString())
+        else:
+            raise NotImplementedError('Saving non-native graph is not supported yet')
 
     @classmethod
-    def load(cls, path:str):
+    def _save_sklearn_graph(cls, graph, path, as_native, prototype):
+        if not as_native:
+            raise NotImplementedError('Saving non-native graph is not supported yet')
+        if hasattr(prototype, 'shape') and hasattr(prototype, 'dtype'):
+            datatype = skl2onnx.common.data_types.guess_data_type(prototype)
+            serialized = skl2onnx.convert_sklearn(graph, initial_types=datatype)
+            cls._save_onnx_graph(serialized, path, as_native)
+        else:
+            raise TypeError(
+                "Serializing scikit learn model needs to know shape and dtype"
+                " of input data which will be inferred from `prototype` "
+                "parameter. It has to be a valid `numpy.ndarray` of shape of your input")
+
+    @classmethod
+    def load(cls, path: str):
         """
         Return the binary data if saved with `as_native` otherwise return the dict
-        that contains binary graph/model on `graph` key. Check `_get_filled_dict`
-        for more details.
+        that contains binary graph/model on `graph` key (Not implemented yet).
         :param path: File path from where the native model or the rai models are saved
         """
         with open(path, 'rb') as f:
