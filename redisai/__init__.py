67492b304c37d2929035d5ea5704c35d2c521b4e
hhsecond <sherin@tensorwerk.com>
2019-06-24 13:19:38 +0530

    new apis with onnxmltools

--- redisai/__init__.py
+++ redisai/__init__.py
@@ -1,22 +1,14 @@
-from .version import __version__
-from .client import (Client, Tensor, BlobTensor, DType, Device, Backend)
+from .version import __version__  # noqa
+from .client import (Client, Tensor, BlobTensor, DType, Device, Backend)  # noqa
+from .model import (  # noqa
+    save_tensorflow, save_torch, save_onnx, save_sklearn, save_sparkml,
+    save_coreml, save_libsvm, save_lightgbm, save_xgboost)
+from .model import load_model  # noqa
 
 
 def save_model(*args, **kwargs):
     """
-    Importing inside to avoid loading the TF/PyTorch/ONNX
-    into the scope unnecessary. This function wraps the
-    internal save model utility to make it user friendly
+    Deprecated
     """
     from .model import Model
     Model.save(*args, **kwargs)
-
-
-def load_model(*args, **kwargs):
-    """
-    Importing inside to avoid loading the TF/PyTorch/ONNX
-    into the scope unnecessary. This function wraps the
-    internal load model utility to make it user friendly
-    """
-    from .model import Model
-    return Model.load(*args, **kwargs)
--- redisai/model.py
+++ redisai/model.py
@@ -1,27 +1,8 @@
 import os
 import warnings
-import sys
 
-try:
-    import tensorflow as tf
-except (ModuleNotFoundError, ImportError):
-    pass
-
-try:
-    import torch
-except (ModuleNotFoundError, ImportError):
-    pass
-
-try:
-    import onnx
-except (ModuleNotFoundError, ImportError):
-    pass
-
-try:
-    import skl2onnx
-    import sklearn
-except (ModuleNotFoundError, ImportError):
-    pass
+from ._util import is_installed
+from ._util import guess_onnx_dtype
 
 
 class Model:
@@ -46,6 +27,9 @@ class Model:
 
     @classmethod
     def save(cls, obj, path: str, input=None, output=None, as_native=True, prototype=None):
+        raise DeprecationWarning((
+            "Single entry for saving different object is deprecated. "
+            "Use specific utility for each type. For more information checkout the documentation"))
         """
         Infer the backend (TF/PyTorch/ONNX) by inspecting the class hierarchy
         and calls the appropriate serialization utility. It is essentially a
@@ -62,83 +46,101 @@ class Model:
             mechanism if True. If False, custom saving utility will be called
             which saves other informations required for modelset. Defaults to True
         """
-        if 'tensorflow' in sys.modules and issubclass(type(obj), tf.Session):
-            cls._save_tf_graph(obj, path, output, as_native)
-        elif 'torch' in sys.modules and issubclass(
-                type(type(obj)), torch.jit.ScriptMeta):
-            # TODO Is there a better way to check this
-            cls._save_torch_graph(obj, path, as_native)
-        elif 'onnx' in sys.modules and issubclass(
-                type(obj), onnx.onnx_ONNX_RELEASE_ml_pb2.ModelProto):
-            cls._save_onnx_graph(obj, path, as_native)
-        elif 'skl2onnx' in sys.modules and issubclass(
-                type(obj), sklearn.base.BaseEstimator):
-            cls._save_sklearn_graph(obj, path, as_native, prototype)
-        else:
-            message = ("Could not find the required dependancy to export the graph object. "
-                       "`save_model` relies on serialization mechanism provided by the"
-                       " supported backends such as Tensorflow, PyTorch, ONNX or skl2onnx. "
-                       "Please install package required for serializing your graph. "
-                       "For more information, checkout the redisia-py documentation")
-            raise RuntimeError(message)
 
-    @classmethod
-    def _save_tf_graph(cls, sess, path, output, as_native):
-        graph_def = sess.graph_def
-        # clearing device information
-        for node in graph_def.node:
-            node.device = ""
-        frozen = tf.graph_util.convert_variables_to_constants(
-            sess, graph_def, output)
-        if as_native:
-            directory = os.path.dirname(path)
-            file = os.path.basename(path)
-            tf.io.write_graph(frozen, directory, file, as_text=False)
-            return
-        else:
-            raise NotImplementedError('Saving non-native graph is not supported yet')
 
-    @classmethod
-    def _save_torch_graph(cls, graph, path, as_native):
-        # TODO how to handle the cpu/gpu
-        if as_native:
-            if graph.training is True:
-                warnings.warn(
-                    'Graph is in training mode. Converting to evaluation mode')
-                graph.eval()
-            torch.jit.save(graph, path)
-            return
-        else:
-            raise NotImplementedError('Saving non-native graph is not supported yet')
+def save_tensorflow(sess, path, output, as_native=False):
+    """
+    TODO: Docstring
+    """
+    if not is_installed('tensorflow'):
+        raise RuntimeError('Please install Tensorflow to use this feature.')
+    import tensorflow as tf
+    graph_def = sess.graph_def
+    # clearing device information
+    for node in graph_def.node:
+        node.device = ""
+    frozen = tf.graph_util.convert_variables_to_constants(
+        sess, graph_def, output)
+    if as_native:
+        directory = os.path.dirname(path)
+        file = os.path.basename(path)
+        tf.io.write_graph(frozen, directory, file, as_text=False)
+        return
+    else:
+        raise NotImplementedError('Saving non-native graph is not supported yet')
 
-    @classmethod
-    def _save_onnx_graph(cls, graph, path, as_native):
-        if as_native:
-            with open(path, 'wb') as f:
-                f.write(graph.SerializeToString())
-        else:
-            raise NotImplementedError('Saving non-native graph is not supported yet')
 
-    @classmethod
-    def _save_sklearn_graph(cls, graph, path, as_native, prototype):
-        if not as_native:
-            raise NotImplementedError('Saving non-native graph is not supported yet')
-        if hasattr(prototype, 'shape') and hasattr(prototype, 'dtype'):
-            datatype = skl2onnx.common.data_types.guess_data_type(prototype)
-            serialized = skl2onnx.convert_sklearn(graph, initial_types=datatype)
-            cls._save_onnx_graph(serialized, path, as_native)
-        else:
-            raise TypeError(
-                "Serializing scikit learn model needs to know shape and dtype"
-                " of input data which will be inferred from `prototype` "
-                "parameter. It has to be a valid `numpy.ndarray` of shape of your input")
+def save_torch(graph, path, as_native=False):
+    """
+    TODO: Docstring
+    """
+    if not is_installed('torch'):
+        raise RuntimeError('Please install PyTorch to use this feature.')
+    import torch
+    # TODO how to handle the cpu/gpu
+    if as_native:
+        if graph.training is True:
+            warnings.warn(
+                'Graph is in training mode. Converting to evaluation mode')
+            graph.eval()
+        torch.jit.save(graph, path)
+        return
+    else:
+        raise NotImplementedError('Saving non-native graph is not supported yet')
 
-    @classmethod
-    def load(cls, path: str):
-        """
-        Return the binary data if saved with `as_native` otherwise return the dict
-        that contains binary graph/model on `graph` key (Not implemented yet).
-        :param path: File path from where the native model or the rai models are saved
-        """
-        with open(path, 'rb') as f:
-            return f.read()
+
+def save_onnx(graph, path, as_native=False):
+    """
+    TODO: Docstring
+    """
+    if as_native:
+        with open(path, 'wb') as f:
+            f.write(graph.SerializeToString())
+    else:
+        raise NotImplementedError('Saving non-native graph is not supported yet')
+
+
+def save_sklearn(graph, path, prototype, as_native=False):
+    """
+    TODO: Docstring
+    """
+    if not is_installed(['onnxmltools', 'skl2onnx', 'pandas']):
+        raise RuntimeError('Please install onnxmltools, skl2onnx, pandas to use this feature.')
+    from onnxmltools import convert_sklearn
+
+    if not as_native:
+        raise NotImplementedError('Saving non-native graph is not supported yet')
+
+    datatype = guess_onnx_dtype(prototype)
+    serialized = convert_sklearn(graph, initial_types=datatype)
+    save_onnx(serialized, path, as_native)
+
+
+def save_sparkml(graph, path, prototype, as_native=False):
+    pass
+
+
+def save_coreml():
+    pass
+
+
+def save_libsvm():
+    pass
+
+
+def save_lightgbm():
+    pass
+
+
+def save_xgboost():
+    pass
+
+
+def load_model(path: str):
+    """
+    Return the binary data if saved with `as_native` otherwise return the dict
+    that contains binary graph/model on `graph` key (Not implemented yet).
+    :param path: File path from where the native model or the rai models are saved
+    """
+    with open(path, 'rb') as f:
+        return f.read()
