dd9e58bc2b19bb7fe87a1848ddafebf9f3da3194
Sandro Cilurzo <sandro.cilurzo@sedimentum.com>
2019-11-29 16:35:22 +0100

    removed autoformatted changes

--- redisai/client.py
+++ redisai/client.py
@@ -10,7 +10,6 @@ class Client(StrictRedis):
     """
     RedisAI client that can call Redis with RedisAI specific commands
     """
-
     def loadbackend(self, identifier: AnyStr, path: AnyStr) -> AnyStr:
         """
         RedisAI by default won't load any backends. User can either explicitly
@@ -21,57 +20,52 @@ class Client(StrictRedis):
         :param path: Path to the shared object of the backend
         :return: byte string represents success or failure
         """
-        return self.execute_command("AI.CONFIG LOADBACKEND", identifier, path)
+        return self.execute_command('AI.CONFIG LOADBACKEND', identifier, path)
 
-    def modelset(
-        self,
-        name: AnyStr,
-        backend: Backend,
-        device: Device,
-        data: ByteString,
-        inputs: Union[AnyStr, Sequence[AnyStr], None] = None,
-        outputs: Union[AnyStr, Sequence[AnyStr], None] = None,
-    ) -> AnyStr:
-        args = ["AI.MODELSET", name, backend.value, device.value]
+    def modelset(self,
+                 name: AnyStr,
+                 backend: Backend,
+                 device: Device,
+                 data: ByteString,
+                 inputs: Union[AnyStr, Sequence[AnyStr], None] = None,
+                 outputs: Union[AnyStr, Sequence[AnyStr], None] = None
+                 ) -> AnyStr:
+        args = ['AI.MODELSET', name, backend.value, device.value]
         if backend == Backend.tf:
-            if not (all((inputs, outputs))):
+            if not(all((inputs, outputs))):
                 raise ValueError(
-                    "Require keyword arguments input and output for TF models"
-                )
-            args += ["INPUTS"] + str_or_strsequence(inputs)
-            args += ["OUTPUTS"] + str_or_strsequence(outputs)
-            args += [data]
-            return self.execute_command(*args)
+                    'Require keyword arguments input and output for TF models')
+            args += ['INPUTS'] + str_or_strsequence(inputs)
+            args += ['OUTPUTS'] + str_or_strsequence(outputs)
+        args += [data]
+        return self.execute_command(*args)
 
     def modelget(self, name: AnyStr) -> dict:
-        rv = self.execute_command("AI.MODELGET", name)
+        rv = self.execute_command('AI.MODELGET', name)
         return {
-            "backend": Backend(to_string(rv[0])),
-            "device": Device(to_string(rv[1])),
-            "data": rv[2],
+            'backend': Backend(to_string(rv[0])),
+            'device': Device(to_string(rv[1])),
+            'data': rv[2]
         }
 
     def modeldel(self, name: AnyStr) -> AnyStr:
-        return self.execute_command("AI.MODELDEL", name)
+        return self.execute_command('AI.MODELDEL', name)
 
-    def modelrun(
-        self,
-        name: AnyStr,
-        inputs: Union[AnyStr, Sequence[AnyStr]],
-        outputs: Union[AnyStr, Sequence[AnyStr]],
-    ) -> AnyStr:
-        args = ["AI.MODELRUN", name]
-        args += ["INPUTS"] + str_or_strsequence(inputs)
-        args += ["OUTPUTS"] + str_or_strsequence(outputs)
+    def modelrun(self,
+                 name: AnyStr,
+                 inputs: Union[AnyStr, Sequence[AnyStr]],
+                 outputs: Union[AnyStr, Sequence[AnyStr]]
+                 ) -> AnyStr:
+        args = ['AI.MODELRUN', name]
+        args += ['INPUTS'] + str_or_strsequence(inputs)
+        args += ['OUTPUTS'] + str_or_strsequence(outputs)
         return self.execute_command(*args)
 
-    def tensorset(
-        self,
-        key: AnyStr,
-        tensor: Union[Tensor, np.ndarray, list, tuple],
-        shape: Union[Sequence[int], None] = None,
-        dtype: Union[DType, None] = None,
-    ) -> Any:
+    def tensorset(self,
+                  key: AnyStr,
+                  tensor: Union[Tensor, np.ndarray, list, tuple],
+                  shape: Union[Sequence[int], None] = None,
+                  dtype: Union[DType, None] = None) -> Any:
         """
         Set the values of the tensor on the server using the provided Tensor object
         :param key: The name of the tensor
@@ -86,35 +80,35 @@ class Client(StrictRedis):
         elif isinstance(tensor, (list, tuple)):
             if shape is None:
                 shape = (len(tensor),)
-                tensor = Tensor(dtype, shape, tensor)
-                args = ["AI.TENSORSET", key, tensor.type.value]
-                args += tensor.shape
-                args += [tensor.ARGNAME]
-                args += tensor.value
-                return self.execute_command(*args)
+            tensor = Tensor(dtype, shape, tensor)
+        args = ['AI.TENSORSET', key, tensor.type.value]
+        args += tensor.shape
+        args += [tensor.ARGNAME]
+        args += tensor.value
+        return self.execute_command(*args)
 
-    def tensorget(
-        self, key: AnyStr, as_type: Type[Tensor] = None, meta_only: bool = False
-    ) -> Union[Tensor, BlobTensor]:
+    def tensorget(self,
+                  key: AnyStr, as_type: Type[Tensor] = None,
+                  meta_only: bool = False) -> Union[Tensor, BlobTensor]:
         """
         Retrieve the value of a tensor from the server. By default it returns the numpy array
         but it can be controlled using `as_type` argument and `meta_only` argument.
         :param key: the name of the tensor
         :param as_type: the resultant tensor type. Returns numpy array if None
         :param meta_only: if true, then the value is not retrieved,
-        only the shape and the type
+            only the shape and the type
         :return: an instance of as_type
         """
         # TODO; We might remove Tensor & BlobTensor in the future and `tensorget` will return
         # python list or numpy arrays or a namedtuple
         if meta_only:
-            argname = "META"
+            argname = 'META'
         elif as_type is None:
             argname = BlobTensor.ARGNAME
         else:
             argname = as_type.ARGNAME
 
-        res = self.execute_command("AI.TENSORGET", key, argname)
+        res = self.execute_command('AI.TENSORGET', key, argname)
         dtype, shape = to_string(res[0]), res[1]
         dt = DType.__members__[dtype.lower()]
         if meta_only:
@@ -125,25 +119,27 @@ class Client(StrictRedis):
             return as_type.from_resp(dt, shape, res[2])
 
     def scriptset(self, name: AnyStr, device: Device, script: AnyStr) -> AnyStr:
-        return self.execute_command("AI.SCRIPTSET", name, device.value, script)
+        return self.execute_command('AI.SCRIPTSET', name, device.value, script)
 
     def scriptget(self, name: AnyStr) -> dict:
-        r = self.execute_command("AI.SCRIPTGET", name)
+        r = self.execute_command('AI.SCRIPTGET', name)
         device = Device(to_string(r[0]))
-        return {"device": device, "script": to_string(r[1])}
+        return {
+            'device': device,
+            'script': to_string(r[1])
+        }
 
     def scriptdel(self, name):
-        return self.execute_command("AI.SCRIPTDEL", name)
+        return self.execute_command('AI.SCRIPTDEL', name)
 
-    def scriptrun(
-        self,
-        name: AnyStr,
-        function: AnyStr,
-        inputs: Union[AnyStr, Sequence[AnyStr]],
-        outputs: Union[AnyStr, Sequence[AnyStr]],
-    ) -> AnyStr:
-        args = ["AI.SCRIPTRUN", name, function, "INPUTS"]
+    def scriptrun(self,
+                  name: AnyStr,
+                  function: AnyStr,
+                  inputs: Union[AnyStr, Sequence[AnyStr]],
+                  outputs: Union[AnyStr, Sequence[AnyStr]]
+                  ) -> AnyStr:
+        args = ['AI.SCRIPTRUN', name, function, 'INPUTS']
         args += str_or_strsequence(inputs)
-        args += ["OUTPUTS"]
+        args += ['OUTPUTS']
         args += str_or_strsequence(outputs)
         return self.execute_command(*args)
--- redisai/tensor.py
+++ redisai/tensor.py
@@ -6,26 +6,26 @@ import numpy as np
 
 
 class Tensor(object):
-    ARGNAME = "VALUES"
+    ARGNAME = 'VALUES'
 
-    def __init__(self, dtype: DType, shape: Sequence[int], value):
-        warnings.warn(
-            "Tensor APIs are depricated and "
-            "will be removed from the future release.",
-            UserWarning,
-        )
+    def __init__(self,
+                 dtype: DType,
+                 shape: Sequence[int],
+                 value):
+        warnings.warn("Tensor APIs are depricated and "
+                      "will be removed from the future release.", UserWarning)
         """
         Declare a tensor suitable for passing to tensorset
         :param dtype: The type the values should be stored as.
-        This can be one of Tensor.FLOAT, tensor.DOUBLE, etc.
+            This can be one of Tensor.FLOAT, tensor.DOUBLE, etc.
         :param shape: An array describing the shape of the tensor. For an
-        image 250x250 with three channels, this would be [250, 250, 3]
+            image 250x250 with three channels, this would be [250, 250, 3]
         :param value: The value for the tensor. Can be an array.
-        The contents must coordinate with the shape, meaning that the
-        overall length needs to be the product of all figures in the
-        shape. There is no verification to ensure that each dimension
-        is correct. Your application must ensure that the ordering
-        is always consistent.
+            The contents must coordinate with the shape, meaning that the
+            overall length needs to be the product of all figures in the
+            shape. There is no verification to ensure that each dimension
+            is correct. Your application must ensure that the ordering
+            is always consistent.
         """
         self.type = dtype
         self.shape = list(shape)
@@ -34,17 +34,19 @@ class Tensor(object):
             self.value = [value]
 
     def __repr__(self):
-        return "<{c.__class__.__name__}(shape={s} type={t}) at 0x{id:x}>".format(
-            c=self, s=self.shape, t=self.type, id=id(self)
-        )
+        return '<{c.__class__.__name__}(shape={s} type={t}) at 0x{id:x}>'.format(
+            c=self,
+            s=self.shape,
+            t=self.type,
+            id=id(self))
 
     @classmethod
-    def from_resp(cls, dtype: DType, shape: Sequence[int], value) -> "Tensor":
+    def from_resp(cls, dtype: DType, shape: Sequence[int], value) -> 'Tensor':
         convert_to_num(dtype, value)
         return cls(dtype, shape, value)
 
     @classmethod
-    def scalar(cls, dtype: DType, *items) -> "Tensor":
+    def scalar(cls, dtype: DType, *items) -> 'Tensor':
         """
         Create a tensor from a list of numbers
         :param dtype: Type to use for storage
@@ -55,14 +57,13 @@ class Tensor(object):
 
 
 class BlobTensor(Tensor):
-    ARGNAME = "BLOB"
+    ARGNAME = 'BLOB'
 
-    def __init__(
-        self,
-        dtype: DType,
-        shape: Sequence[int],
-        *blobs: Union["BlobTensor", ByteString]
-    ):
+    def __init__(self,
+                 dtype: DType,
+                 shape: Sequence[int],
+                 *blobs: Union['BlobTensor', ByteString]
+                 ):
         """
         Create a tensor from a binary blob
         :param dtype: The datatype, one of Tensor.FLOAT, Tensor.DOUBLE, etc.
@@ -74,22 +75,22 @@ class BlobTensor(Tensor):
             for b in blobs:
                 if isinstance(b, BlobTensor):
                     b = b.value[0]
-                    blobarr += b
-                    size = len(blobs)
-                    ret_blobs = bytes(blobarr)
-                    shape = [size] + list(shape)
-                else:
-                    ret_blobs = bytes(blobs[0])
+                blobarr += b
+            size = len(blobs)
+            ret_blobs = bytes(blobarr)
+            shape = [size] + list(shape)
+        else:
+            ret_blobs = bytes(blobs[0])
 
         super(BlobTensor, self).__init__(dtype, shape, ret_blobs)
 
     @classmethod
-    def from_numpy(cls, *nparrs) -> "BlobTensor":
+    def from_numpy(cls, *nparrs) -> 'BlobTensor':
         blobs = []
         for arr in nparrs:
             blobs.append(arr.data)
-            dt = DType.__members__[str(nparrs[0].dtype)]
-            return cls(dt, nparrs[0].shape, *blobs)
+        dt = DType.__members__[str(nparrs[0].dtype)]
+        return cls(dt, nparrs[0].shape, *blobs)
 
     @property
     def blob(self):
@@ -103,11 +104,14 @@ class BlobTensor(Tensor):
     def _to_numpy_type(t):
         if isinstance(t, DType):
             t = t.value
-            mm = {"FLOAT": "float32", "DOUBLE": "float64"}
-            if t in mm:
-                return mm[t]
+        mm = {
+            'FLOAT': 'float32',
+            'DOUBLE': 'float64'
+        }
+        if t in mm:
+            return mm[t]
         return t.lower()
 
     @classmethod
-    def from_resp(cls, dtype, shape, value) -> "BlobTensor":
+    def from_resp(cls, dtype, shape, value) -> 'BlobTensor':
         return cls(dtype, shape, value)
