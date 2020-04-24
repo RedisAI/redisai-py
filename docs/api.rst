74c030f32935a7e918c82d39cba7bcf9c8fadbb4
hhsecond <sherin@tensorwerk.com>
2020-04-24 14:30:13 +0530

    docs setup

---
+++ docs/api.rst
@@ -0,0 +1,12 @@
+.. _ref-api:
+
+API Documentation
+=================
+redisai-py is the python client for `RedisAI <http://redisai.io/>`_. This documentation hosts documentation of
+all user-facing APIs. The only entrypoint the users should be using is the ``Client`` class
+
+
+Client Class
+------------
+.. autoclass:: redisai.Client
+   :members:
---
+++ docs/conf.py
@@ -0,0 +1,34 @@
+import redisai
+
+project = 'redisai-py'
+copyright = '2020, RedisLabs'
+author = 'RedisLabs'
+release = redisai.__version__
+extensions = ['sphinx.ext.autodoc',
+              'sphinx.ext.autosummary',
+              'sphinx.ext.extlinks',
+              'sphinx.ext.napoleon',
+              'sphinx.ext.todo',
+              'sphinx.ext.intersphinx',
+              "sphinx_rtd_theme"]
+templates_path = ['_templates']
+source_suffix = '.rst'
+master_doc = 'index'
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+html_theme = 'sphinx_rtd_theme'
+html_use_smartypants = True
+html_last_updated_fmt = '%b %d, %Y'
+html_split_index = False
+html_sidebars = {
+   '**': ['searchbox.html', 'globaltoc.html', 'sourcelink.html'],
+}
+html_short_title = '%s-%s' % (project, release)
+
+napoleon_use_ivar = True
+napoleon_use_rtype = True
+napoleon_use_param = True
+napoleon_include_init_with_doc = True
+
+add_module_names = False
+doctest_test_doctest_blocks = None
+autoclass_content = 'class'
