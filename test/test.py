4ca3dfd3a98cbf9a8bb0301976a4637a12864193
alonre24 <alonreshef24@gmail.com>
2021-05-19 14:31:15 +0300

    Make fixes in tests to suit the change in "AI.modelget"

--- test/test.py
+++ test/test.py
@@ -153,6 +153,7 @@ class ClientTestCase(RedisAITestBase):
                 "device": "cpu",
                 "inputs": ["a", "b"],
                 "minbatchsize": 0,
+                "minbatchtimeout": 0
                 "outputs": ["mul"],
                 "tag": "v1.0",
             },
@@ -261,6 +262,7 @@ class ClientTestCase(RedisAITestBase):
                 "device": "cpu",
                 "inputs": ["a", "b"],
                 "minbatchsize": 0,
+                "minbatchtimeout": 0,
                 "outputs": ["mul"],
                 "tag": "v1.0",
             },
