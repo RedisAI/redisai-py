b59772e4bb369dd8a102740eacb7ba5fc8e98ab7
AvitalFineRedis <avital.fine@redislabs.com>
2021-08-05 14:51:16 +0300

    readthedocs changes

--- docs/conf.py
+++ docs/conf.py
@@ -10,7 +10,11 @@ extensions = [
     "sphinx.ext.todo",
     "sphinx.ext.intersphinx",
     "sphinx_rtd_theme",
+    'sphinx_search.extension',  # search tools
+    'sphinx.ext.autodoc',
 ]
+pygments_style = "sphinx"
+autoapi_type = 'python'
 templates_path = ["_templates"]
 source_suffix = ".rst"
 master_doc = "index"
@@ -19,6 +23,7 @@ html_theme = "sphinx_rtd_theme"
 html_use_smartypants = True
 html_last_updated_fmt = "%b %d, %Y"
 html_split_index = False
+html_static_path = ['_static']
 html_sidebars = {
     "**": ["searchbox.html", "globaltoc.html", "sourcelink.html"],
 }
